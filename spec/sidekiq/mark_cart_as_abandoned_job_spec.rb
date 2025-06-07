675f6e223983561faa48cca2c9b44afa940e4d8e
Matheus Oliveira <matheus.oliveirav@gmail.com>
2025-06-07 15:25:55 -0300

    Refactor job test

--- spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
+++ spec/sidekiq/mark_cart_as_abandoned_job_spec.rb
@@ -3,47 +3,53 @@ require 'rails_helper'
 RSpec.describe MarkCartAsAbandonedJob, type: :job do
   include ActiveSupport::Testing::TimeHelpers
 
+  around do |example|
+    freeze_time { example.run }
+  end
+
   describe '#perform' do
-    let!(:active_cart) { create(:cart, :active) }
-    let!(:to_be_marked_cart) { create(:cart, :to_be_marked_as_abandoned) }
-    let!(:freshly_abandoned_cart) { create(:cart, :freshly_abandoned) }
-    let!(:to_be_removed_cart) { create(:cart, :to_be_removed) }
 
     it 'marks eligible carts as abandoned' do
+      cart = create(:cart, :to_be_marked_as_abandoned)
       expect {
         described_class.new.perform
-      }.to change { to_be_marked_cart.reload.abandoned }.from(false).to(true)
+      }.to change { cart.reload.abandoned }.from(false).to(true)
     end
 
     it 'does not mark active carts' do
+      cart = create(:cart, :active)
       described_class.new.perform
-      expect(active_cart.reload.abandoned).to be_falsey
+      expect(cart.abandoned).to be_falsey
     end
 
     it 'does not mark already abandoned carts' do
+      cart = create(:cart, :freshly_abandoned)
       expect {
         described_class.new.perform
-      }.not_to change { freshly_abandoned_cart.reload.abandoned }
+      }.not_to change { cart.reload.abandoned }
     end
 
     it 'removes old abandoned carts' do
+      cart = create(:cart, :to_be_removed)
       expect {
         described_class.new.perform
-      }.to change { Cart.exists?(to_be_removed_cart.id) }.from(true).to(false)
+      }.to change { Cart.exists?(cart.id) }.from(true).to(false)
     end
 
     it 'does not remove recently abandoned carts' do
+      cart = create(:cart, :freshly_abandoned)
       described_class.new.perform
-      expect(Cart.exists?(freshly_abandoned_cart.id)).to be true
+      expect(Cart.exists?(cart.id)).to be true
     end
   end
 
   describe 'scheduling' do
-    before { Sidekiq::Worker.clear_all }
     it 'enqueues the job' do
-      expect {
-        described_class.perform_async
-      }.to change { Sidekiq::Queues['default'].size }.by(1)
+      Sidekiq::Testing.fake! do
+        expect {
+          described_class.perform_async
+        }.to change { Sidekiq::Queues['default'].size }.by(1)
+      end
     end
 
     it 'runs successfully when enqueued' do
